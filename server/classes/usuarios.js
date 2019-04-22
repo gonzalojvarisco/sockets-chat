342692799043521d6d9a36ae486f0db14a026001
gonzalo <gonzalojvarisco@gmail.com>
2019-04-22 16:05:20 -0300

    inicio

---
+++ server/classes/usuarios.js
@@ -0,0 +1,53 @@
+
+
+
+class Usuarios {
+
+    constructor(){
+        this.personas = [];
+    }
+
+    agregarPersona(id, nombre){
+
+        let persona = { id, nombre };
+
+        this.personas.push(persona);
+
+        return this.personas;
+
+    }
+
+    getPersona(id){
+
+        let persona = this.personas.filter(pers => pers.id === id)[0]; //filter va a verificar en el arreglo si hay una persona con el mismo id. El [0] se coloca para que solo devuelva un elemento
+        
+        return persona; //si no encuentra una persona va a devolver undefine o null
+    }
+
+    getPersonas(){
+        return this.personas;
+    }
+
+    getPersonasPorSala(sala){
+        //....
+    }
+
+    borrarPersona(id){
+
+        let personaBorrada  = this.getPersona(id);
+
+        this.personas = this.personas.filter(pers =>{
+            return pers.id != id;
+        });
+
+        return personaBorrada;
+
+    }
+
+}
+
+
+
+module.exports = {
+    Usuarios
+}
